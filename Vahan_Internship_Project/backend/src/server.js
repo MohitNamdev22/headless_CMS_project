3600351b6f1d0f656595bc00c1ffd2fc622b46c9
Mohit Namdev <namdevmohit0@gmail.com>
2024-05-11 23:57:38 +0530

    create route

--- Vahan_Internship_Project/backend/src/server.js
+++ Vahan_Internship_Project/backend/src/server.js
@@ -1,8 +1,10 @@
 const mysql = require('mysql2');
 const config = require('../config/config');
-
 const express = require('express');
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 // Create a database connection pool
 const pool = mysql.createPool(config.database);
@@ -21,6 +23,25 @@ pool.getConnection((err, connection) => {
 // server.js (or app.js)
 
 // Define route for fetching all entities
+
+app.post('/api/entities', (req, res) => {
+  console.log("post started")
+  const { name, email, mobileNumber, dateOfBirth } = req.body; // Assuming request body contains entity data
+  console.log("taken data from body")
+  const sql = 'INSERT INTO entities (name, email, mobileNumber, dateOfBirth) VALUES (?, ?, ?, ?)';
+  console.log("query run")
+  pool.query(sql, [name, email, mobileNumber, dateOfBirth], (err, result) => {
+    if (err) {
+      console.error('Error creating entity:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    console.log('Entity created successfully!');
+    res.status(201).send('Entity created successfully!');
+  });
+});
+
+
 app.get('/api/entities', (req, res) => {
   // Fetch all entities from the database
   const sql = 'SELECT * FROM entities';
